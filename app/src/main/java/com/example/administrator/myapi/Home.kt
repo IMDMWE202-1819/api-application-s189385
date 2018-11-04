264dde73fcc48a6c1dd77cd50bbad179c886f05b
s189385 <s189385@uos.ac.uk>
2018-11-04 01:38:01 +0000

    Adding navigation functionality and alo adding klaxon library

--- app/src/main/java/com/example/administrator/myapi/Home.kt
+++ app/src/main/java/com/example/administrator/myapi/Home.kt
@@ -1,12 +1,22 @@
 package com.example.administrator.myapi
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val MySearchButton = findViewById<Button>(R.id.searchbutton)
+
+        MySearchButton.setOnClickListener{
+            intent = Intent(this, searchandresults :: class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/administrator/myapi/searchandresults.kt
@@ -0,0 +1,15 @@
+package com.example.administrator.myapi
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.beust.klaxon.Klaxon
+
+
+class searchandresults : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_searchandresults)
+    }
+    
+}
