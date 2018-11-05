04b82167eee50dc11ed3920d051ba7822a3a59af
darryljf <darryl.jade.frost@gmail.com>
2018-11-05 10:46:16 +0000

    Adding adapter for recycler view and making minor changes to layout files

---
+++ app/src/main/java/com/example/administrator/myapi/MyAdapter.kt
@@ -0,0 +1,9 @@
+package layout
+
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+
+
+
+
+
--- app/src/main/java/com/example/administrator/myapi/searchandresults.kt
+++ app/src/main/java/com/example/administrator/myapi/searchandresults.kt
@@ -2,7 +2,12 @@ package com.example.administrator.myapi
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.widget.Button
+import android.widget.TextView
 import com.beust.klaxon.Klaxon
+import kotlinx.android.synthetic.main.activity_searchandresults.*
+import org.w3c.dom.Text
 
 
 class searchandresults : AppCompatActivity() {
@@ -10,6 +15,18 @@ class searchandresults : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_searchandresults)
+
+        val MySubmitButton = findViewById<Button>(R.id.submitbutton)
+        val MyRecyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+
+        MySubmitButton.setOnClickListener{
+            val ArtistName = findViewById<TextView>(R.id.artistname).text.toString()
+            println(ArtistName)
+        }
     }
+
+
+
+
     
 }
